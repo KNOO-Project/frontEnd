87211a31401958f738000bc775346ff77fdf57a9
shchoon <acckr0604@gmail.com>
2023-07-10 20:30:05 +0900

    kakao map 오버레이 여러개 마커 표시 연습

--- src/category/restaurant/restaurant.js
+++ src/category/restaurant/restaurant.js
@@ -13,7 +13,6 @@ function Restaurant(){
     //console.log(params['*'] === '' || params['*'] === '신관');
     //console.log(params['*'] === '신관');
     const { kakao } = window;    
-
     useEffect(() => {
         var container = document.getElementById('map');
         let mapCoor;
@@ -25,11 +24,10 @@ function Restaurant(){
         
         var options = { //지도를 생성할 때 필요한 기본 옵션
             center: mapCoor, //지도의 중심좌표.
-            level: 3 //지도의 레벨(확대, 축소 정도)
+            level: 4 //지도의 레벨(확대, 축소 정도)
         };
         
         var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
-
         var markerPosition  = mapCoor; 
 
         // 마커를 생성합니다
@@ -55,8 +53,65 @@ function Restaurant(){
         
         var resultDiv = document.getElementById('clickLatlng'); 
         resultDiv.innerHTML = message;
-    
-    });
+         
+            });
+        
+        // 마커를 표시할 위치와 내용을 가지고 있는 객체 배열입니다 
+        var positions = [
+            {                                                                               //신관 천안 포지션 구별해서 적용하기
+                content: '<div>다복수육국밥</div>', 
+                latlng: new kakao.maps.LatLng(36.4732522, 127.1415602)
+            },
+            {
+                content: '<div>롯데리아</div>', 
+                latlng: new kakao.maps.LatLng(36.4730706, 127.1407595)
+            },
+            {
+                content: '<div>파스꾸치</div>', 
+                latlng: new kakao.maps.LatLng(36.473489, 127.1409026)
+            },
+            {
+                content: '<div>스타벅스</div>', 
+                latlng: new kakao.maps.LatLng(36.4679803, 127.1427074)
+            },
+            {
+                content: '<div>도스마스</div>', 
+                latlng: new kakao.maps.LatLng(36.4709681, 127.1380492)
+            }
+        ];
+
+        for (var i = 0; i < positions.length; i ++) {
+            // 마커를 생성합니다
+            var marker = new kakao.maps.Marker({
+                map: map, // 마커를 표시할 지도
+                position: positions[i].latlng // 마커의 위치
+            });
+
+            // 마커에 표시할 인포윈도우를 생성합니다 
+            var infowindow = new kakao.maps.InfoWindow({
+                content: positions[i].content // 인포윈도우에 표시할 내용
+            });
+
+            // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+            // 이벤트 리스너로는 클로저를 만들어 등록합니다 
+            // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+            kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+            kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+        }
+
+        // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+        function makeOverListener(map, marker, infowindow) {
+            return function() {
+                infowindow.open(map, marker);
+            };
+        }
+
+        // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+        function makeOutListener(infowindow) {
+            return function() {
+                infowindow.close();
+            };
+        }    
         
 
         //console.log('mapCoor', mapCoor);
@@ -68,6 +123,8 @@ function Restaurant(){
         <div>
             <div className="map_box">
                 <div id="map" ></div>
+                
+
                 <div className="map_select">
                     <Link className="shinkwan" to='신관' >신관캠퍼스</Link>
                     <br/>
