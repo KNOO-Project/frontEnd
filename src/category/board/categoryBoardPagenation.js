4af4c90a2f385af1aec817e0c1c458c9ceb80f5f
shchoon <acckr0604@gmail.com>
2023-08-18 18:07:16 +0900

    categoryPagenation UI 변경

--- src/category/board/categoryBoardPagenation.js
+++ src/category/board/categoryBoardPagenation.js
@@ -6,8 +6,7 @@ import {useEffect, useState} from 'react';
 import BoardDetail from "./boardDetail";
 import axios from "axios";
 import {TbCircleChevronLeft, TbCircleChevronRight} from 'react-icons/tb';
-import {AiOutlineSearch} from 'react-icons/ai';
-import {HiPencil} from 'react-icons/hi';
+import {AiOutlineLike, AiOutlineStar, AiOutlineComment} from 'react-icons/ai';
 
 function CategoryBoardPagenation(props){
     var token = props.token;
@@ -127,15 +126,21 @@ function CategoryBoardPagenation(props){
                     </form>
                 </div> */}
                 <div className="board_list">
-                    {boardData.map((a, i) => {
+                    {boardData.map((data, i) => {
                         while(i < 20){
                             return(
-                                <Link to={`../detail/${a.post_id}`} key={i}>   
-                                    <div className="title">{a.post_title}</div>
-                                    <div className="content">{a.post_content.substring(0, 20)}</div> {/* 본문내용 20자만 보여주기 */}
-                                    <div className="date">{a.post_date}</div>
-                                    <div className="name">{a.writer_name}</div>
-                                    <hr></hr>
+                                <Link to={`../detail/${data.post_id}`} key={i}>   
+                                    <div className="title">{data.post_title}</div>
+                                    <div className="content">{data.post_content.substring(0, 20)}</div> {/* 본문내용 20자만 보여주기 */}
+                                    <div className="date">{data.post_date}</div>
+                                    <div className="name">{data.writer_name}</div>
+                                    <div style={{clear: 'both'}}></div>
+                                    <div className="counts">
+                                        <li><AiOutlineLike />{data.likes_count}</li>
+                                        <li><AiOutlineComment />{data.comments_count}</li>
+                                        <li><AiOutlineStar />{data.scraps_count}</li>
+                                    </div>
+                                    {data.thumbnail ? <img src={data.thumbnail} alt="" /> : null}
                                 </Link>
                             )
                         }
