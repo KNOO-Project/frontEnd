684aeadc439d10b4299357690de22f61864679c6
shchoon <acckr0604@gmail.com>
2023-09-05 17:32:43 +0900

    setupProxy.js 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
   app.use(
-      createProxyMiddleware('/api', {
+      createProxyMiddleware('http://210.125.212.192:8888/api', {
           target: 'https://knoo.netlify.app/',
           changeOrigin: true
       })
