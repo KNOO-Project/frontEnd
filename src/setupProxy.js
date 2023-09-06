f0268e14f54fc1614681c7937b738bf1e213220e
shchoon <acckr0604@gmail.com>
2023-09-06 16:35:18 +0900

    setupProxy.js pathRewrite 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
   app.use(
-      createProxyMiddleware('http://210.125.212.192:8888/api', {
+      createProxyMiddleware('/api', {
           target: 'https://knoo.netlify.app/',
-          changeOrigin: true
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' }
       })
   )
 };
