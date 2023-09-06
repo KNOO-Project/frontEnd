174a4a837a789501fcae7563928e7b38646b850a
shchoon <acckr0604@gmail.com>
2023-09-06 21:20:50 +0900

    setupProxy.js 파일 추가

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      // 백엔드 주소
+      target: "http://210.125.212.192:8888",
+      changeOrigin: true,
+    })
+  );
+};
